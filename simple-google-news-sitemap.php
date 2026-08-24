0833194f9617c610ca838282d4b040474b830614
Peter Wilson <git-commit-by@peterwilson.cc>
2026-08-24 14:29:34 +1000

    Avoid use of reserved term.

--- simple-google-news-sitemap.php
+++ simple-google-news-sitemap.php
@@ -31,17 +31,17 @@ if ( ! defined( 'ABSPATH' ) ) {
  * PSR-4 autoloading
  */
 spl_autoload_register(
-	function ( $class ) {
+	function ( $class_name ) {
 		// Project-specific namespace prefix.
 		$prefix = 'SimpleGoogleNewsSitemap\\';
 		// Base directory for the namespace prefix.
 		$base_dir = __DIR__ . '/includes/classes/';
 		// Does the class use the namespace prefix?
 		$len = strlen( $prefix );
-		if ( strncmp( $prefix, $class, $len ) !== 0 ) {
+		if ( strncmp( $prefix, $class_name, $len ) !== 0 ) {
 			return;
 		}
-		$relative_class = substr( $class, $len );
+		$relative_class = substr( $class_name, $len );
 		$file           = $base_dir . str_replace( '\\', '/', $relative_class ) . '.php';
 		// If the file exists, require it.
 		if ( file_exists( $file ) ) {
